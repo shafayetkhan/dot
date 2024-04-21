6f1db3cda15d07f196277e7daf2951210f85128f
Shafayet Khan <shafayetkhan@gmail.com>
2024-04-21 00:10:44 -0600

    Add lazygit config and theme

--- config/fish/conf.d/theme.fish
+++ config/fish/conf.d/theme.fish
@@ -1,3 +1,5 @@
 fish_config theme choose "Catppuccin Mocha"
 
 set -x BAT_THEME Catppuccin-mocha 
+
+set -x LG_CONFIG_FILE "$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/themes/catppuccin/mocha/blue.yml"
