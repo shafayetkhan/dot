ce9854b5da02389a3658485753851b5eb9d84a36
Shafayet Khan <shafayetkhan@gmail.com>
2024-04-20 22:40:05 -0600

    Borrowed some config from omerxx
    Mostly basic changes like setting theme, font size. I like the window decoration style.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -13,12 +13,31 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "tokyonight"
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+--- config.color_scheme = "tokyonight"
+config.color_scheme = "Catppuccin Mocha"
+-- enable_tab_baxr = false
+config.font_size = 16.0
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.window_background_opacity = 0.90
-config.macos_window_background_blur = 20
+config.window_background_opacity = 1.0
+config.macos_window_background_blur = 30
 
+config.keys = {
+	{
+		key = "f",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
 -- and finally, return the configuration to wezterm
 return config
